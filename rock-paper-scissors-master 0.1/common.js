0cb76bb946401af7abde2be12075483a64299bbd
김지수 <73218962+jisoo03010@users.noreply.github.com>
2022-02-08 12:16:39 +0900

    Add files via upload

--- rock-paper-scissors-master 0.1/common.js
+++ rock-paper-scissors-master 0.1/common.js
@@ -1,11 +1,19 @@
 // NOTE: 변수 초기화
 let rules = document.getElementById("rules")
+
 let popup = document.getElementById("popup")
+
 let contents = document.getElementById("contents")
+
 let playgame = document.getElementById("playgame")
+
+
 let paper = document.getElementById("paper")
+
 let scisors = document.getElementById("scisors")
+
 let rock = document.getElementById("rock")
+
 
 // NOTE: 팝업 오픈
 rules.addEventListener("click", function pop() {
@@ -23,14 +31,19 @@ close_icon.addEventListener("click", function cl() {
 function pagemove() {
     let my = document.getElementById("my")
     contents.style.display = "none"
+
     playgame.style.display = "flex"
 
     let paper_random = document.getElementById("paper_random")
+
     let scisors_random = document.getElementById("scisors_random")
+
     let rock_random = document.getElementById("rock_random")
 
     let paper_randomhose = document.getElementById("paper_randomhose")
+
     let scisors_randomhose = document.getElementById("scisors_randomhose")
+
     let rock_randomhose = document.getElementById("rock_randomhose")
 
 
@@ -42,24 +55,30 @@ function pagemove() {
             let result = document.getElementById("result")
             var imgArray = new Array();
             imgArray[0] = paper_randomhose;
+
             imgArray[1] = scisors_random;
+
             imgArray[2] = rock_random;
 
             var imgNum = Math.floor(Math.random() * 3)
+
             let randomappend = random.appendChild(imgArray[imgNum])
 
             if (randomappend == paper_randomhose) {
                 result.innerText = "YOU 비김"
+
                 paper_randomhose.style.display = "block"
 
             } else if (randomappend == scisors_random) {
                 console.log('졌습니다')
+
                 result.innerText = "YOU LOSE"
                 random.appendChild(scisors_random).className += "waves"
             } else {
 
                 console.log('이겼습니다')
                 result.innerText = "YOU WIN"
+
                 result.style.left = 417 + "px"
                 my.appendChild(paper_random).className += "waves"
 
@@ -69,20 +88,30 @@ function pagemove() {
         }, 1000)
 
     } else if (event.currentTarget == scisors) {
+
         my.appendChild(scisors_random)
 
+
+
+
         setTimeout(function randomimg() {
+
             let random = document.getElementById("random")
+
             let result = document.getElementById("result")
             var imgArray = new Array();
+
             imgArray[0] = paper_random;
             imgArray[1] = scisors_randomhose;
+            
             imgArray[2] = rock_random;
 
             var imgNum = Math.floor(Math.random() * 3)
+            
             let randomappend = random.appendChild(imgArray[imgNum])
 
             let score_num = document.getElementById("score_num")
+
             if (randomappend == paper_random) {
                 console.log('이겼습니다')
                 result.innerText = "YOU WIN"
@@ -91,10 +120,12 @@ function pagemove() {
 
 
             } else if (randomappend == scisors_randomhose) {
+
                 console.log('비겼습니다')
                 result.innerText = "YOU 비김"
                 scisors_randomhose.style.display = "block"
             } else {
+
                 console.log('졌습니다')
                 result.innerText = "YOU LOSE"
                 random.appendChild(rock_random).className += "waves"
@@ -105,28 +136,38 @@ function pagemove() {
         // my.style.right = 53 + "vw"
 
     } else if (event.currentTarget == rock) {
+
         my.appendChild(rock_random)
 
         setTimeout(function randomimg() {
+
             let random = document.getElementById("random")
             let result = document.getElementById("result")
+
             var imgArray = new Array();
             imgArray[0] = paper_random;
             imgArray[1] = scisors_random;
             imgArray[2] = rock_randomhose;
 
             var imgNum = Math.floor(Math.random() * 3)
+
             let randomappend = random.appendChild(imgArray[imgNum])
+
             if (randomappend == paper_random) {
+
                 console.log('졌습니다')
                 result.innerText = "YOU LOSE"
                 random.appendChild(paper_random).className += "waves"
             } else if (randomappend == scisors_random) {
+
                 console.log('이겼습니다')
-                result.innerText = "YOU WIN"
+               result.innerText = "YOU WIN"
+
+
                 result.style.left = 417 + "px"
 
                 my.appendChild(rock_random).className += "waves"
+            
 
             } else {
                 console.log('비겼습니다')
@@ -147,8 +188,16 @@ function pagemove() {
 // let playAgain_btn = document.getElementById("playAgain_btn")
 
 // //게임을 다시 시작할때
+
+
+
 // playAgain_btn.addEventListener("click", function () {
 //     playgame.style.display = "none"
+
+
+
+
+
 //     contents.style.display = "block"
 
 // })
@@ -156,6 +205,10 @@ function pagemove() {
 
 //     playgame.querySelector("#my>div").className += ""
 //     playgame.querySelector("#my").style.display = "none"
+
+
+
+
 
 //     playgame.querySelector("#random>div").style.display = "none"
 //     if (document.getElementsByClassName("waves")) {
